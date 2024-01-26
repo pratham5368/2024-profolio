f8d346b2060d721531e0d8cd8d894147b82fe304
pratham rai <97867973+pratham5368@users.noreply.github.com>
2024-01-26 05:53:00 +0530

    50%done

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a href='https://twitter.com/prathamrai0'>Twitter</a>
+        <a href='https://www.instagram.com/pratham_0021_/'>Instagram</a>
+        <a href='https://codepen.io/pratham-rai'>Codepen</a>
+        <a href='https://www.linkedin.com/in/prathamrai01/'>LinkedIn</a>
+    </div>
+  )
+}
